bf49a00723f2707438bc8e5b671e9769e5b6c681
vbdevru <vbdevru@mail.ru>
2023-12-17 17:03:06 +0300

    #2 Basic program, var, const

---
+++ basics/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	// message := "Я скоро стану GOLANG NINJA"
+	// const message string = "Я скоро стану GOLANG NINJA"
+	// var message string
+	// message = "Я скоро стану GOLANG NINJA"
+	// var message = "Я скоро стану GOLANG NINJA"
+	// var number float32
+	// var b bool
+
+	// number = 17.9
+	// b = true
+
+	// message := []byte("asd")
+	// var a rune = 'A'
+
+	// fmt.Println(a)
+	// fmt.Println(number)
+	// fmt.Println(b)
+
+	a, b, c := 1, 2, 3
+
+	a, _, c = 1, 7, 3
+
+	fmt.Println(a, b, c)
+}

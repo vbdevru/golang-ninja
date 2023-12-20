f740b94a59883e4e86c6759d8c7e0de23d433cfb
vbdevru <vbdevru@mail.ru>
2023-12-20 14:20:33 +0300

    #9 Array, slice, matrix, for

--- basics/main.go
+++ basics/main.go
@@ -1,27 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
-	number := 5
-	var p *int
+	// messages := [5]string{"1", "2", "3"}
 
-	p = &number
-	// message := "Скоро я стану ниндзя!"
+	// messages := []string{"1", "2", "3"}
 
-	fmt.Println(p)
-	fmt.Println(&number)
+	// var messages []string
 
-	*p = 10
+	// printMessages(messages)
 
-	fmt.Println(number)
+	// messages := make([]string, 100)
 
-	//changeMessage(message)
+	// messages[0] = "1"
 
-	//fmt.Println(&message)
+	// messages = append(messages, "6")
+	// messages = append(messages, "7")
+	// messages = append(messages, "8")
+	// messages = append(messages, "9")
+	// messages = append(messages, "10")
+
+	// fmt.Println(messages)
+    // fmt.Println(len(messages))
+	// fmt.Println(cap(messages))
+	// messages = append(messages, "101")
+	// fmt.Println(len(messages))
+	// fmt.Println(cap(messages))
+
+	matrix := make([][]int, 10)
+
+	counter := 0
+	for x := 0; x < 10; x++ {
+		matrix[x] = make([]int, 10)
+		for y := 0; y < 10; y++ {
+			counter++
+			matrix[x][y] = counter
+		}
+		fmt.Println(matrix[x])
+	} 
+
 }
 
-func changeMessage(message string) {
-	// *message += " (из функции printMessage())"
-	fmt.Println(&message)
+func printMessages(messages []string) error {
+	if len(messages) == 0 {
+		return errors.New("empty array")
+	}
+
+	messages[1] = "5"
+
+	fmt.Println(messages)
+
+	return nil
 }

1ec7d98f33062f7576fb0fa96fcf79fc3b13e1d6
vbdevru <vbdevru@mail.ru>
2023-12-20 12:46:34 +0300

    #5 for, range e, ...args

--- basics/main.go
+++ basics/main.go
@@ -1,75 +1,23 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-)
+import "fmt"
 
 func main() {
-<<<<<<< HEAD
-	// var message string
-	// message = sayHello("Максим", 21)
-	// printMessage(message)
-	// printMessage("вызов 1")
-	// printMessage("вызов 2")
-	// printMessage("вызов 3")
-	// mesage, err := enterTheClub(12)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
-	fmt.Println(prediction("rrgrtg"))
-=======
-	var message string
-	message = sayHello("Максим", 21)
-	printMessage(message)
-	printMessage("вызов 1")
-	printMessage("вызов 2")
-	printMessage("вызов 3")
-	mesage, err := enterTheClub(12)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(mesage)
->>>>>>> 62091d5c1fa39ef57490d8106510a6018ff690bc
+	fmt.Println(findMax(1, 2, 3, 5623, 1234, -2, 53, -55, 25252, 252))
 }
 
-func printMessage(message string) {
-	fmt.Println(message)
-}
-
-func sayHello(name string, age int) string {
-	return fmt.Sprintf("Привет, %s! Тебе %d лет", name, age)
-}
-
-func enterTheClub(age int) (string, error) {
-	if age >= 18 && age <= 45 {
-		return "Входи, только аккуратно", nil
-	} else if age >= 45 && age < 65 {
-		return "Вам точно понравится эта музыка?", nil
-	} else if age >= 65 {
-		return "Это уже слишком для вас", errors.New("you are too old")
+func findMax(numbers ...int) int {
+	if len(numbers) == 0 {
+		return 0
 	}
 
-	return "Тебе нет 18-ти", errors.New("you are too young")
-	
-<<<<<<< HEAD
-}
+	max := numbers[0]
 
-func prediction(dayOfWeek string) (string,error) {
-	switch dayOfWeek {
-	case "пн":
-		return "Хорошего тебе начала недели!", nil
-	case "вт":
-		return "Прекрасного тебе вторника!", nil
-	case "ср":
-		return "Замечательной тебе среды!", nil
-	case "чт":
-		return "Четверг - это маленькая пятница! Только не переборщи!", nil
-	default:
-		return "невалидный день недели", errors.New("invalid day of the week")
+	for _, i := range numbers {
+		if i < max { 
+			max = i
+		}
 	}
-=======
->>>>>>> 62091d5c1fa39ef57490d8106510a6018ff690bc
+
+	return max
 }

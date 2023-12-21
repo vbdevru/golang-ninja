a309980552da14a80a581d9247ad8301424b638a
vbdevru <vbdevru@mail.ru>
2023-12-21 14:10:02 +0300

    #17 gorutines, sync.WaitGroup, Add(), Wait(), Done()

--- concurrency/main.go
+++ concurrency/main.go
@@ -1,23 +1,102 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"sync"
+	"time"
+)
 
-var b int
-
-func main() {
-	a := createPtr()
-
-	fmt.Println(*a)
-
-	*a = 10
-
-	fmt.Println(*a)
+var actions = []string{
+	"logged in",
+	"logged out",
+	"create record",
+	"update record",
 }
 
-func createPtr() *int {
-	//a := new(int)
-	//var a *int
+type logItem struct {
+	action string
+	timestamp time.Time
+}
 
-	a := 5
-	return &a
+type User struct {
+	id int
+	email string
+	logs []logItem
+}
+
+func (u User) getActivityInfo() string {
+	out := fmt.Sprintf("ID:%d | Email: %s\nActivity Log:\n", u.id, u.email)
+	for i, item := range u.logs {
+		out += fmt.Sprintf("%d. [%s] at %s\n", i+1, item.action, item.timestamp)
+	}
+
+	return out
+}
+
+func main() {
+	rand.Seed(time.Now().Unix())
+
+	t := time.Now()
+
+	wg := &sync.WaitGroup{}
+
+	users := generateUsers(1000)
+
+	for _, user := range users {
+		wg.Add(1)
+		go saveUserInfo(user, wg)
+	}
+
+	wg.Wait()
+
+	fmt.Println("TIME ELAPSED:", time.Since(t).String())
+} 
+
+func saveUserInfo(user User, wg *sync.WaitGroup) error {
+	time.Sleep(time.Millisecond * 10)
+	fmt.Printf("WRITING FILE FOR USER ID: %d\n", user.id)
+
+	filename := fmt.Sprintf("logs/uid_%d.txt", user.id)
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		return err
+	}
+
+	_, err = file.WriteString(user.getActivityInfo())
+	if err != nil {
+		return err
+	}
+
+	wg.Done()
+
+	return nil
+}
+
+func generateUsers(count int) []User {
+	users := make([]User, count)
+
+	for i := 0; i < count; i++ {
+		users[i] = User{
+			id: i + 1,
+			email: fmt.Sprintln("user%d@ninja.go", i+1),
+			logs: generateLogs(rand.Intn(1000)),
+		}
+	}
+
+	return users
+}
+
+func generateLogs(count int) []logItem {
+	logs := make([]logItem, count)
+
+	for i := 0; i < count; i++ {
+		logs[i] = logItem{
+			timestamp: time.Now(),
+			action: actions[rand.Intn(len(actions) - 1)],
+		}
+	}
+
+	return logs
 }
